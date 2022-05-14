300b76e89962073722a44bfc307e653ad1c44aa2
yashas a bharadwaj <yashasbharadwaj9@gmail.com>
2022-05-14 23:56:55 +0530

    connected to database

--- summione/summione/settings.py
+++ summione/summione/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'account',
+    'contact',
 ]
 
 MIDDLEWARE = [
@@ -74,10 +76,17 @@ WSGI_APPLICATION = 'summione.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+'default': {
+'ENGINE': 'django.db.backends.mysql',
+'NAME': 'summione',
+'USER': 'root',
+'PASSWORD': "",
+'HOST': "",
+'PORT': "",
+'OPTIONS': {
+'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+}
+}
 }
 
 
